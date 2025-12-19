6bb4fed6ee8cb482d9662b4aa01b32375105e00a
bala-amypo <bala@amypo.in>
2025-12-19 17:32:59 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,19 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Bin Overflow Predictor API")
-                        .version("1.0")
-                        .description("API for managing bins and predicting overflow"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9257.pro604cr.amypo.ai/")
+                ));
+        }
 }
