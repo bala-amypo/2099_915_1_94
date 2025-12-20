f6419bdb62993d218174e6b8969044a2a94e1bce
bala-amypo <bala@amypo.in>
2025-12-20 10:05:41 +0000

    User commit

--- src/main/java/com/example/demo/util/WeekendUtil.java
+++ src/main/java/com/example/demo/util/WeekendUtil.java
@@ -1,5 +1,12 @@
 package com.example.demo.util;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class WeekendUtil {
-    
+
+    public static boolean isWeekend(LocalDate date) {
+        DayOfWeek day = date.getDayOfWeek();
+        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+    }
 }
