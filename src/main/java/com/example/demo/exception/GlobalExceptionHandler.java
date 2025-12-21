d8bec93d0964c1cd763f2106f4d62029a8ec9ee7
bala-amypo <bala@amypo.in>
2025-12-21 13:10:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.badRequest().body(
+            Map.of(
+                "timestamp", LocalDateTime.now(),
+                "error", ex.getMessage()
+            )
+        );
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            Map.of(
+                "timestamp", LocalDateTime.now(),
+                "error", ex.getMessage()
+            )
+        );
+    }
+}
